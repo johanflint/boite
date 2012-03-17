0acc87133b67fb02b9bd91ef874537cd2f6cbdd8
Jan Ouwens <jan.ouwens@nummulus.com>
2012-03-17 17:23:04 +0100

    Renamed withMessage to saying; made it match partial messages

--- boite-test/src/test/scala/com/nummulus/boite/scalatest/FailMatcherTest.scala
+++ boite-test/src/test/scala/com/nummulus/boite/scalatest/FailMatcherTest.scala
@@ -11,7 +11,7 @@ import org.scalatest.matchers.ShouldMatchers
 
 @RunWith(classOf[JUnitRunner])
 class FailMatcherTest extends FlatSpec with ShouldMatchers {
-  val aMessage    = "message"
+  val aMessage    = "this is the exception message"
   val anException = new IllegalStateException(aMessage)
   
   val aFull    = Full("foo")
@@ -35,14 +35,24 @@ class FailMatcherTest extends FlatSpec with ShouldMatchers {
   }
   
   "FailureBePropertyMatcher#withMessage" should "correctly match a message" in {
-    aFailure should be a (failure withMessage aMessage)
-    aFailure should not be a (failure withMessage "something else")
-    anEmpty  should not be a (failure withMessage aMessage)
+    aFailure should be a (failure saying aMessage)
+  }
+  
+  it should "not match a failure with another message" in {
+    aFailure should not be a (failure saying "something else")
+  }
+  
+  it should "not match a non-failure" in {
+    anEmpty  should not be a (failure saying aMessage)
+  }
+  
+  it should "match a partial message" in {
+    aFailure should be a (failure saying "message")
   }
   
   it should "give an appropriate error message" in {
     val thrown = intercept[TestFailedException] {
-      anEmpty should be a (failure withMessage aMessage)
+      anEmpty should be a (failure saying aMessage)
     }
     thrown.getMessage should include ("failure with message \"" + aMessage + "\"")
   }
