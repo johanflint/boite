8f2888edbc57b59760d9e2942c5ec6f3c03d5c58
Johan Flint <johan.flint@nummulus.com>
2012-08-13 22:55:57 +0200

    BTE-15 Minor fixes to BoxTest to conform to the style guide

--- boite/src/test/scala/com/nummulus/boite/BoxTest.scala
+++ boite/src/test/scala/com/nummulus/boite/BoxTest.scala
@@ -28,16 +28,18 @@ class BoxTest extends FlatSpec with ShouldMatchers {
   val Exception = new UnsupportedOperationException(NotSupported)
   val Error = new AssertionError
   
-  "wrap" should "return Empty when it is passed null" in {
+  behavior of "Box.wrap"
+  
+  it should "return Empty when it is passed null" in {
     Box wrap { null } should be (Empty)
   }
   
   it should "return Full(value) when it is passed a value" in {
-    Box wrap { GilbertBecaud } should equal (Full(GilbertBecaud))
+    Box wrap { GilbertBecaud } should be (Full(GilbertBecaud))
   }
   
   it should "return Failure when an exception is thrown while evaluating the expression passed to it" in {
-    Box wrap { throw Exception } should equal (Failure(Exception))
+    Box wrap { throw Exception } should be (Failure(Exception))
   }
   
   it should "let Errors pass through" in {
@@ -46,11 +48,13 @@ class BoxTest extends FlatSpec with ShouldMatchers {
     }
   }
   
-  "apply" should "accept an empty Option and return Empty" in {
-    Box(None) should equal (Empty)
+  behavior of "Box.apply"
+  
+  it should "accept an empty Option and return Empty" in {
+    Box(None) should be (Empty)
   }
   
   it should "accept a full Option and return Full" in {
-    Box(Some(GilbertBecaud)) should equal (Full(GilbertBecaud))
+    Box(Some(GilbertBecaud)) should be (Full(GilbertBecaud))
   }
 }
