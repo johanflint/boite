e1ee1f37c057ca4c18f9315bc0f22920b7ac1419
Johan Flint <johan.flint@nummulus.com>
2012-08-13 23:55:01 +0200

    BTE-15 Minor fixes to matcher tests to conform to the style guide

--- boite-scalatest/src/test/scala/com/nummulus/boite/scalatest/FullMatcherTest.scala
+++ boite-scalatest/src/test/scala/com/nummulus/boite/scalatest/FullMatcherTest.scala
@@ -26,41 +26,41 @@ import org.scalatest.junit.JUnitRunner
 class FullMatcherTest extends BoxMatcherTestTrait {
   "apply" should { 
     "match a full" in {
-      aFull should be a full
+      FullWithFoo should be a full
     }
     
     "not match non-failures" in {
-      anEmpty should not be a (full)
-      aFailure should not be a (full)
+      Empty should not be a (full)
+      FailureWithException should not be a (full)
     }
     
     "give an appropriate error message" in {
       itShouldFailSaying("not a full") {
-        anEmpty should be a full
+        Empty should be a full
       }
     }
   }
   
   "containing" should {
     "correctly match an object" in {
-      aFull should be a (full containing "foo")
+      FullWithFoo should be a (full containing "foo")
     }
     
     "not match a full with another object" in {
-      aFull should not be a (full containing "another object")
+      FullWithFoo should not be a (full containing "another object")
     }
     
     "not match a full with an object of another class" in {
-      aFull should not be a (full containing 0)
+      FullWithFoo should not be a (full containing 0)
     }
     
     "not match a non-failure" in {
-      anEmpty should not be a (full containing "foo")
+      Empty should not be a (full containing "foo")
     }
     
     "give an appropriate error message" in {
       itShouldFailSaying("full containing \"foo\"") {
-        anEmpty should be a (full containing "foo")
+        Empty should be a (full containing "foo")
       }
     }
   }
