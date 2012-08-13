252a54eae3e444013762e4a374d10f3e5f720f0a
Johan Flint <johan.flint@nummulus.com>
2012-08-14 00:09:40 +0200

    BTE-15 Rewrote matcher tests to use FlatSpec instead of WordSpec

--- boite-scalatest/src/test/scala/com/nummulus/boite/scalatest/FullMatcherTest.scala
+++ boite-scalatest/src/test/scala/com/nummulus/boite/scalatest/FullMatcherTest.scala
@@ -21,47 +21,49 @@ package scalatest
 import BoiteMatchers._
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.FlatSpec
 
 @RunWith(classOf[JUnitRunner])
-class FullMatcherTest extends BoxMatcherTestTrait {
-  "apply" should { 
-    "match a full" in {
-      FullWithFoo should be a full
-    }
-    
-    "not match non-failures" in {
-      Empty should not be a (full)
-      FailureWithException should not be a (full)
-    }
-    
-    "give an appropriate error message" in {
-      itShouldFailSaying("not a full") {
-        Empty should be a full
-      }
+class FullMatcherTest extends FlatSpec with BoxMatcherTestTrait {
+  behavior of "apply"
+  
+  it should "match a full" in {
+    FullWithFoo should be a full
+  }
+  
+  it should "not match non-failures" in {
+    Empty should not be a (full)
+    FailureWithException should not be a (full)
+  }
+  
+  it should "give an appropriate error message" in {
+    itShouldFailSaying("not a full") {
+      Empty should be a full
     }
   }
   
-  "containing" should {
-    "correctly match an object" in {
-      FullWithFoo should be a (full containing "foo")
-    }
-    
-    "not match a full with another object" in {
-      FullWithFoo should not be a (full containing "another object")
-    }
-    
-    "not match a full with an object of another class" in {
-      FullWithFoo should not be a (full containing 0)
-    }
-    
-    "not match a non-failure" in {
-      Empty should not be a (full containing "foo")
-    }
-    
-    "give an appropriate error message" in {
-      itShouldFailSaying("full containing \"foo\"") {
-        Empty should be a (full containing "foo")
-      }
+  
+  behavior of "containing"
+  
+  it should "correctly match an object" in {
+    FullWithFoo should be a (full containing "foo")
+  }
+  
+  it should "not match a full with another object" in {
+    FullWithFoo should not be a (full containing "another object")
+  }
+  
+  it should "not match a full with an object of another class" in {
+    FullWithFoo should not be a (full containing 0)
+  }
+  
+  it should "not match a non-failure" in {
+    Empty should not be a (full containing "foo")
+  }
+  
+  it should "give an appropriate error message" in {
+    itShouldFailSaying("full containing \"foo\"") {
+      Empty should be a (full containing "foo")
     }
   }
 }
